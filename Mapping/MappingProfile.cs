a486269889b6a472f93e44bcb35cc1cd25a88e5b
Daniel Breen <dapperdandev@gmail.com>
2018-05-15 15:23:55 -0600

    Fix Issue Where Updating Cases Sometimes Fails and/or Deletes Children

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -17,22 +17,34 @@ namespace ToucanTesting.Mapping
         {
             CreateMap<TestSuite, TestSuiteResource>();
             CreateMap<TestSuiteResource, TestSuite>();
+
             CreateMap<TestRun, TestRunResource>();
             CreateMap<TestRunResource, TestRun>();
+
             CreateMap<TestModule, TestModuleResource>();
             CreateMap<TestModuleResource, TestModule>()
                 .ForMember(c => c.Id, opt => opt.Ignore());
+
             CreateMap<TestCase, TestCaseResource>();
             CreateMap<TestCaseResource, TestCase>()
-                .ForMember(c => c.Id, opt => opt.Ignore());
+                .ForMember(c => c.Id, opt => opt.Ignore())
+                .ForMember(c => c.TestIssues, opt => opt.Ignore())
+                .ForMember(c => c.TestActions, opt => opt.Ignore())
+                .ForMember(c => c.ExpectedResults, opt => opt.Ignore())
+                .ForMember(c => c.TestConditions, opt => opt.Ignore());
+
             CreateMap<TestAction, TestActionResource>();
             CreateMap<TestActionResource, TestAction>();
+
             CreateMap<ExpectedResult, ExpectedResultResource>();
             CreateMap<ExpectedResultResource, ExpectedResult>();
+
             CreateMap<TestCondition, TestConditionResource>();
             CreateMap<TestConditionResource, TestCondition>();
+
             CreateMap<TestResult, TestResultResource>();
             CreateMap<TestResultResource, TestResult>();
+            
             CreateMap<TestIssue, TestIssueResource>();
             CreateMap<TestIssueResource, TestIssue>();
 

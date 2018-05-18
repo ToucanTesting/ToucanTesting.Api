f862d8a152c9daec53f32cdc13bea89503a8c246
Daniel Breen <dapperdandev@gmail.com>
2018-05-18 07:18:56 -0600

    Add HasCriteria

---
+++ Migrations/20180518124848_AddHasCriteriaToTestCase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ToucanTesting.Migrations
+{
+    public partial class AddHasCriteriaToTestCase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasCriteria",
+                table: "TestCases",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasCriteria",
+                table: "TestCases");
+        }
+    }
+}
